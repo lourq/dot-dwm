9941df44b084f8895b07eb76c9b91f48b2d1ffba
Your Name <you@example.com>
2023-07-18 16:05:00 +0300

    update config.h 18-07-23

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Source Code Pro:size=11",
-					                   "Material Icons Round:size=11"
-									  };
+					                            "Material Icons Round:size=11"};
 static const char dmenufont[]       = "Source Code Pro:size=11";
 static const char col_gray1[]       = "#101010";
 static const char col_gray2[]       = "#101010";
@@ -29,7 +28,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 1,       -1,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 2,       1,           -1 },
+	{ "steam",    NULL,       NULL,       1 << 3,       1,           -1 },
+	{ "obs",      NULL,       NULL,       1 << 4,       -1,           -1 },
 };
 
 /* layout(s) */
@@ -64,7 +66,7 @@ static const char *termcmd[]  = { "kitty", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,            		XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,            		        XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -72,7 +74,7 @@ static const Key keys[] = {
 	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	//{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
